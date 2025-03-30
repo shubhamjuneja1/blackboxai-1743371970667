30389f89555c6e78104dce69b646daa8aeb9296e
Shubham Juneja <76601207+shubhamjuneja1@users.noreply.github.com>
2025-03-31 03:30:34 +0530

    Add ClothRentalApp/App.tsx 1743372033501

---
+++ ClothRentalApp/App.tsx
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return <AppNavigator />;
+}
